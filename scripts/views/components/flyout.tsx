c1bbeaa608b8ff5046d3d524aa33bf9704db8a7c
Joe Mbaya <Joe@atHomes-MacBook-Pro.local>
2016-05-20 10:29:45 +0200

    Flout!

---
+++ scripts/views/components/flyout.tsx
@@ -0,0 +1,21 @@
+
+ 
+import * as React from 'react';
+
+interface IFlyoutProps {
+    
+}
+
+interface IFlyoutState {
+    
+}
+
+export class  Flyout extends React.Component<IFlyoutProps, IFlyoutState> {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (<div class="col-md-12"></div>);
+  }
+}
+
